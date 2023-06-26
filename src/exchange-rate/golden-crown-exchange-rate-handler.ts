37f26c437e3c65ae544435ba90857da7dec53347
Max Kalyakin <kalyakin.maks@mail.ru>
2023-06-27 00:12:27 +0400

    fix types

--- src/exchange-rate/golden-crown-exchange-rate-handler.ts
+++ src/exchange-rate/golden-crown-exchange-rate-handler.ts
@@ -7,5 +7,7 @@ interface GoldenCrownExchangeRateResponse {
 export async function getGoldenCrownExchangeRate(): Promise<GoldenCrownExchangeRateResponse> {
 	const response = await fetch('https://koronapay.com/transfers/online/api/transfers/tariffs?sendingCountryId=RUS&sendingCurrencyId=810&receivingCountryId=GEO&receivingCurrencyId=981&paymentMethod=debitCard&receivingAmount=100&receivingMethod=cash&paidNotificationEnabled=false');
 
-	return (await response.json()) as GoldenCrownExchangeRateResponse;
+	const currencyRates = (await response.json()) as GoldenCrownExchangeRateResponse[];
+
+	return currencyRates[0];
 }
