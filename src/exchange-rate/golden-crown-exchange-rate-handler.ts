a4fba6eaea6642690266bbed1b76dc2f167f7d21
mkalyakin <mkalyakin@tradingview.com>
2024-07-13 13:31:15 +0400

    add logs

--- src/exchange-rate/golden-crown-exchange-rate-handler.ts
+++ src/exchange-rate/golden-crown-exchange-rate-handler.ts
@@ -5,9 +5,15 @@ interface GoldenCrownExchangeRateResponse {
 }
 
 export async function getGoldenCrownExchangeRate(): Promise<GoldenCrownExchangeRateResponse> {
+	console.log('make GC request');
+
 	const response = await fetch('https://koronapay.com/transfers/online/api/transfers/tariffs?sendingCountryId=RUS&sendingCurrencyId=810&receivingCountryId=GEO&receivingCurrencyId=981&paymentMethod=debitCard&receivingAmount=100&receivingMethod=cash&paidNotificationEnabled=false');
 
+	console.log('got GC response, parsing...');
+
 	const currencyRates = (await response.json()) as GoldenCrownExchangeRateResponse[];
+
+	console.log('response parsed');
 
 	return currencyRates[0];
 }
