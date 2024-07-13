24b532ce4cac9620c4bb3d30785be9b02edf38d3
mkalyakin <mkalyakin@tradingview.com>
2024-07-13 13:47:54 +0400

    use native fetch

--- src/exchange-rate/golden-crown-exchange-rate-handler.ts
+++ src/exchange-rate/golden-crown-exchange-rate-handler.ts
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 interface GoldenCrownExchangeRateResponse {
 	exchangeRate: number;
 }
--- src/telegram/message-sender.ts
+++ src/telegram/message-sender.ts
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 interface Dependencies {
 	botToken: string;
 	chatId: number;
